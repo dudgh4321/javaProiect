d1a574e1042536254c0f7329de73f119265c6293
user <user@DESKTOP-2AI31C4>
2021-02-21 22:33:49 +0900

    02.21

--- javaProject/src/bankSystem/Account.java
+++ javaProject/src/bankSystem/Account.java
@@ -1,17 +1,42 @@
 package bankSystem;
 
 public class Account {
+	static int index = 0;
 	private String accNum;
 	private String name;
-	private int balance;
+	int balance;
 	
 	public Account(String accNum, String name, int balance) {
 		
 		this.accNum = accNum;
 		this.name = name;
+		this.balance = 0;
+
+	
+	}
+
+	public String getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(String accNum) {
+		this.accNum = accNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
 		this.balance = balance;
-		
-		
 	}
 	
 }
